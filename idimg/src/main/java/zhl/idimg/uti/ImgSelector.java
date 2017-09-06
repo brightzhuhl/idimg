5f0fb92f2e873e66164a1a25df635f2a645aad94
Administrator <Administrator@PC-201705161019>
2017-09-06 18:12:25 +0800

    滴水算法初步

--- idimg/src/main/java/zhl/idimg/uti/ImgSelector.java
+++ idimg/src/main/java/zhl/idimg/uti/ImgSelector.java
@@ -1,29 +1,38 @@
-package zhl.idimg;
+package zhl.idimg.uti;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetAdapter;
 import java.awt.dnd.DropTargetDropEvent;
 import java.io.File;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class CharNumClassifier extends JFrame {
+public class ImgSelector extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
-	private JPanel charNumPanel;
 
 	private JPanel infoPanel;
+	
+	private JButton btnBar;
 
-	private HashSet<File> files;
+	private HashSet<File> files = new HashSet<>();
+	
+	private HashMap<Object, JLabel> itemMap = new HashMap<>();
+	
+	private Object lock = new Object();
 
-	public CharNumClassifier() {
+	public ImgSelector() {
 		super();
 		setDropTarget(new DropTarget(this, DnDConstants.ACTION_COPY_OR_MOVE, new DropTargetAdapter() {
 			@Override
@@ -50,6 +59,21 @@ public class CharNumClassifier extends JFrame {
 				}
 			}
 		}));
+		setSize(500, 400);
+		setLayout(new FlowLayout());
+		infoPanel = new JPanel();
+		infoPanel.setSize(500,320);
+		infoPanel.setVisible(true);
+		add(infoPanel);
+		
+		btnBar = new JButton("确定");
+		btnBar.addActionListener(e->{
+			synchronized (lock) {
+				lock.notifyAll();
+			}
+			this.setVisible(false);
+		});
+		add(btnBar);
 	}
 	
 	public void addFile(File file){
@@ -58,9 +82,27 @@ public class CharNumClassifier extends JFrame {
 			if(name.endsWith(".bmp") && !name.endsWith("cn.bmp")){
 				if(!files.contains(file)){
 					files.add(file);
+					itemMap.put(file, addInfoItem(file.getPath()));
 				}
 			}
 		}
-		
 	}
-}
+	
+	public JLabel addInfoItem(String name){
+		JLabel label = new JLabel(name);
+		infoPanel.add(label);
+		return label;
+	}
+	
+	public File[] getSelectFiles(){
+		synchronized (lock) {
+			try {
+				lock.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return files.toArray(new File[files.size()]);
+	}
+	
+ }
